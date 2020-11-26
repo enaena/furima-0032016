7064482a3b2acf9ffd0860ea113d2ff1f54d48db
enaena <e_ngu_no1@yahoo.co.jp>
2020-11-26 13:44:21 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :cotegory_id, presence: true
+  validates :condition_id, presence: true
+  validates :bueden, presence: true
+  validates :area, presence: true
+  validates :day, presence: true
+  validates :price, presence: true
+
+  validates :price, format: { with: /^[a-zA-Z0-9]+$/ }
+  validates_inclusion_of :price, in:300..9999999
+
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
