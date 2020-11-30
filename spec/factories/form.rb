b0f49dc672b29fa1e372f417bb7512e318b077a1
enaena <e_ngu_no1@yahoo.co.jp>
2020-11-30 19:17:12 +0900

    レビュー修正_05

--- spec/factories/form.rb
+++ spec/factories/form.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     postal_code    { '000-0000' }
     area_id        { 2 }
     municipality   { '横浜市緑区' }
-    house_number   { '青山1-1-1Z' }
+    house_number   { '青山1-1-1' }
+    building_name  { 'アーバン' }
     phone_number   { '00000000000' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
