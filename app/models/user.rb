d11e7ad1247c9ce1e82996bab6f11297985e8b6a
enaena <e_ngu_no1@yahoo.co.jp>
2020-12-01 14:14:51 +0900

    挙動確認修正_01

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
   validates :nickname, presence: true
 
